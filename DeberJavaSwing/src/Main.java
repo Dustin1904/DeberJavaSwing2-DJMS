92a271c7575a2b8482a3c07524311dd58d3ab9e9
Dustin <dustin04ms@gmail.com>
2024-01-27 20:13:48 -0500

    xd

--- DeberJavaSwing/src/Main.java
+++ DeberJavaSwing/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Login");
+        frame.setContentPane(new form1().Login);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
     }
 }
---
+++ DeberJavaSwing/src/form1.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class form1 {
+    JPanel Login;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JLabel Img;
+    private JLabel Log;
+    private JLabel User;
+    private JLabel Pass;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        Img = new JLabel(new ImageIcon("Fondo.jpg"));
+    }
+}
